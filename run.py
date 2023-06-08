a0b20db138322417d32fa0fb0cbb00f5a4ff119f
Arthur Nangai <arthur.nangai@gmail.com>
2023-06-08 13:24:28 +0300

    Update docker file to use python

---
+++ run.py
@@ -0,0 +1,14 @@
+import subprocess
+
+# Define the path to your bash script
+script_path = '.wg++/run.sh'
+
+# Start the bash script using subprocess
+process = subprocess.Popen(['bash', script_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+# Wait for the script to finish and capture the output
+stdout, stderr = process.communicate()
+
+# Print the output
+print("Standard Output:\n", stdout.decode())
+print("Standard Error:\n", stderr.decode())
