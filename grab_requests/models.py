031823150b2f865326b1c52c7bd899ea6ef30aaf
Arthur Nangai <arthur.nangai@gmail.com>
2023-06-12 13:27:56 +0300

    Update tasks to record result log

--- grab_requests/models.py
+++ grab_requests/models.py
@@ -1,20 +1,24 @@
 from django.db import models
 from common.models import BaseTimeStampedModel
+from enum import Enum
 
-REQUEST_STATUS = ['PENDING', 'COMPLETE', 'ERROR']
+class RequestStatusEnum(Enum):
+    PENDING = 'PENDING'
+    COMPLETE = 'COMPLETE'
+    ERROR = 'ERROR'
+
 
 class GrabRequest(BaseTimeStampedModel, models.Model):
     """
     A grab request is a request to get a programming guide
     from a given site/channel.
     """
-    REQUEST_STATUS_CHOICES=models.TextChoices('Request_Status_Choices', " ".join(REQUEST_STATUS))
     site=models.CharField(max_length=255)
     site_id=models.CharField(max_length=255)
     xmltv_id=models.CharField(max_length=255)
     result_xml=models.TextField(blank=True)
     result_log = models.TextField(blank=True)
     status=models.CharField(
-        max_length=50, choices=REQUEST_STATUS_CHOICES.choices, default="PENDING",
+        max_length=50, choices=[(choice, choice.value) for choice in RequestStatusEnum], default="PENDING",
     )
     site_name=models.CharField(max_length=255, blank=True)  # sometimes site_name might be different from xmltv_id
--- grab_requests/tasks.py
+++ grab_requests/tasks.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 from utils.xml_utils import create_config_xml
 from celery import shared_task
-from grab_requests.models import GrabRequest
+from grab_requests.models import GrabRequest, RequestStatusEnum
 from common.custom_logging import logger
 
 
@@ -34,16 +34,22 @@ def run_web_grab(
     stdout, stderr = process.communicate()
 
     # Print the output
-    print("Standard Output:\n", stdout.decode())
+    standard_output = stdout.decode()
+    standard_error = stderr.decode()
+    print("Standard Output:\n", standard_output)
     print("Standard Error:\n", stderr.decode())
 
     try:
         with open('.wg++/guide.xml', 'r') as reader:
             grab_request = GrabRequest.objects.get(id=request_id)
-            # Read & print the entire file
-            logger.info('Updating webgrab request')
+            logger.info('Updating webgrab request: %s', request_id)
             grab_request.result_xml=reader.read()
+            grab_request.status = RequestStatusEnum.COMPLETE.value
+            grab_request.result_log=f'{str(standard_output)} \n {str(standard_error)}'
             grab_request.save()
-            print(reader.read())
     except FileNotFoundError:
         logger.info('Grabber failed to create guide.xml for request: %s: %s', request_id, xmltv_id)
+        grab_request = GrabRequest.objects.get(id=request_id)
+        grab_request.status = RequestStatusEnum.ERROR.value
+        grab_request.result_log=f'{str(standard_output)} \n {str(standard_error)}'
+        grab_request.save()
