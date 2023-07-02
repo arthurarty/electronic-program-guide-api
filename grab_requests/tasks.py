35d47e8c87ddd27c83b766d08ead1fc784d2edd2
Arthur Nangai <arthur.nangai@gmail.com>
2023-07-02 21:11:22 +0300

    make changes to handle reading the icon tag

--- grab_requests/tasks.py
+++ grab_requests/tasks.py
@@ -1,6 +1,7 @@
 import json
 import os
 import subprocess
+import xml.etree.ElementTree as ET
 from typing import Optional
 
 import requests
@@ -11,6 +12,7 @@ from rest_framework import serializers
 from common.custom_logging import logger
 from grab_requests.models import GrabRequest, RequestStatusEnum
 from utils.xml_utils import create_config_xml
+
 
 load_dotenv()
 
@@ -28,8 +30,29 @@ def send_call_back(grab_request: GrabRequest):
     return requests.post(call_back_url, data=json.dumps(serializer.data), timeout=60)
 
 
+def get_icon_tag(xml_str: str) -> str:
+    root = ET.fromstring(xml_str)
+    channel_tags = root.findall('.//channel')
+    for channel_tag in channel_tags:
+        return channel_tag.find('icon')
+
+
 @shared_task()
 def run_web_grab(
+    request_id: int,
+    site: str,
+    site_id: str,
+    xmltv_id: str,
+    channel_name: Optional[str] = None,
+    offset: Optional[str] = None,
+):
+    # if offset:
+    #     # if offset is set, we need to run the grabber twice.
+    #     _run_web_grab(request_id, site, site_id, xmltv_id, channel_name, offset)
+    _run_web_grab(request_id, site, site_id, xmltv_id, channel_name, None)
+
+
+def _run_web_grab(
     request_id: int,
     site: str,
     site_id: str,
@@ -65,11 +88,15 @@ def run_web_grab(
             grab_request = GrabRequest.objects.get(id=request_id)
             logger.info('Updating webgrab request: %s', request_id)
             grab_request.result_xml=reader.read()
-            grab_request.status = RequestStatusEnum.COMPLETE.value
-            grab_request.result_log=f'{str(standard_output)} \n {str(standard_error)}'
+            if not offset:
+                grab_request.status = RequestStatusEnum.COMPLETE.value
+                grab_request.result_log=f'{str(standard_output)} \n {str(standard_error)}'
+            else:
+                grab_request.icon_tag=get_icon_tag(reader.read())
             grab_request.save()
             logger.info('Done Updating now hitting the callback')
-            response = send_call_back(grab_request)
+            if not offset:
+                response = send_call_back(grab_request)
             logger.info(response)
     except FileNotFoundError:
         logger.info('Grabber failed to create guide.xml for request: %s: %s', request_id, xmltv_id)
