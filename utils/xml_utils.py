a21a5dc217277ab81ab1460d2e7419c9acdc999a
Arthur Nangai <arthur.nangai@gmail.com>
2023-07-13 20:59:00 +0300

    remove a print statement

--- utils/xml_utils.py
+++ utils/xml_utils.py
@@ -80,7 +80,6 @@ def create_config_xml(
         additional_tags=additional_tags,
         guide_name=guide_name,
     )
-    print(xml_str)
     root = ET.fromstring(xml_str)
     tree = ET.ElementTree(root)
     logger.info('Writing config file %s', file_path)
