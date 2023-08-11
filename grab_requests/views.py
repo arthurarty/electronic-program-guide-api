6882bdd506f5746878600b902ac39419b81852bc
Arthur Nangai <arthur.nangai@gmail.com>
2023-08-11 18:45:22 +0300

    use a print statement instead of a log

--- grab_requests/views.py
+++ grab_requests/views.py
@@ -27,8 +27,8 @@ class GrabRequestBulkCreateView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        logger.info(request.data)
-        logger.info(serializer.errors)
+        print(request.data)
+        print(serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
