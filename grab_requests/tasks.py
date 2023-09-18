b6fb8fb0f589165720af14c225d82041310ed73a
Arthur Nangai <arthur.nangai@gmail.com>
2023-09-18 20:43:52 +0300

    Return delete of tasks

--- grab_requests/tasks.py
+++ grab_requests/tasks.py
@@ -77,7 +77,7 @@ def run_web_grab(
     resp = send_call_back(grab_request, timeout)
     if grab_request:
         logger.info('Delete grab request %s}', grab_request.id)
-        # grab_request.delete()
+        grab_request.delete()
     logger.info(resp.status_code)
     logger.info(resp.text)
 
